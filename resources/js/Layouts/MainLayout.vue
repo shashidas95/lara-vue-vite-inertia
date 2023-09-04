8f4ef8d230d0b1ad8ef19ba707b8e6166bdce271
shashidas <shashidas95@gmail.com>
2023-09-04 08:38:25 +0600

    0

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,8 @@
+<template>
+    <Link  href="/"> mainlayout </Link>&nbsp;
+    <Link  href="/hello"> showlayout </Link>
+    <slot></slot>
+</template>
+<script setup>
+import {Link} from '@inertiajs/vue3'
+</script>
