3a8e92dfcf2f0ddd2398086949da9195c2e7d00f
shashidas <shashidas95@gmail.com>
2023-09-05 08:03:24 +0600

    inertia-middlewire

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,8 +1,8 @@
 <template>
-    <Link  href="/"> mainlayout </Link>&nbsp;
-    <Link  href="/hello"> showlayout </Link>
-    <slot></slot>
+    <Link href="/"> Main Page </Link>&nbsp;
+    <Link href="/hello"> Show page </Link>
+    <slot>Default</slot>
 </template>
 <script setup>
-import {Link} from '@inertiajs/vue3'
+import { Link } from "@inertiajs/vue3";
 </script>
