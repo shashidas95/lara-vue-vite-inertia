986225b5d09e0ce977585d4e1aedc14eab597d6f
shashidas <shashidas95@gmail.com>
2023-09-09 20:03:36 +0600

    Delete

--- resources/js/pages/Listing/Index.vue
+++ resources/js/pages/Listing/Index.vue
@@ -10,6 +10,11 @@
         <div>
             <Link :href="`/listing/${listing.id}/edit`"> Edit</Link>
         </div>
+        <div>
+            <Link :href="`/listing/${listing.id}`" method="delete">
+                Delete</Link
+            >
+        </div>
     </div>
 </template>
 
