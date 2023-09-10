94c525100df1fa928ed6a69c2dfcd23649bb7f73
shashidas <shashidas95@gmail.com>
2023-09-10 18:28:24 +0600

    basic style with tailwind

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,21 +1,39 @@
 <template>
-    <Link href="/listing"> All Listing </Link>&nbsp;
-    <Link href="/listing/create"> New Listing </Link>
-    <div v-if="flashSuccess" class="success">
-        {{ flashSuccess }}
-    </div>
     <Header
-        class="border-b border-gray-200 bg-white dark:bg-gray-900 text-gray-800 dark:text-gray-300"
+        class="border-b border-gray-200 bg-white dark:bg-gray-700 text-gray-800 dark:text-gray-300 w-full"
     >
         <div class="container mx-auto">
             <nav class="p-4 flex items-center justify-between">
-                <div>X</div>
-                <div>Y</div>
-                <div>Z</div>
+                <div
+                    class="text-lg font-medium bg-indigo-800 hover:bg-indigo-500 dark:bg-indigo-600 text-white p-2 rounded-md"
+                >
+                    <Link :href="route('listing.index')"> All Listing </Link>
+                </div>
+                <div
+                    class="text-xl text-indigo-600 dark:text-indigo-300 font-medium text-center"
+                >
+                    <Link :href="route('listing.index')">
+                        Laravel-Vue-Vite-Inertia
+                    </Link>
+                </div>
+                <div
+                    class="text-lg font-medium bg-indigo-800 hover:bg-indigo-500 dark:bg-indigo-600 text-white p-2 rounded-md"
+                >
+                    <Link :href="route('listing.create')"> +New Listing </Link>
+                </div>
             </nav>
         </div>
     </Header>
-    <slot> Default </slot>
+
+    <main class="container mx-auto p-4">
+        <div
+            v-if="flashSuccess"
+            class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-800 p-2 text-center"
+        >
+            {{ flashSuccess }}
+        </div>
+        <slot> Default </slot>
+    </main>
 </template>
 <script setup>
 import { computed } from "vue";
